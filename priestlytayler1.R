c118c6e3a304396a1c7179a245b56c94817d304b
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2021-08-03 22:46:28 -0400

    Update priestlytayler1.R

--- priestlytayler1.R
+++ priestlytayler1.R
@@ -1,26 +1,29 @@
 library(terra)
-library(dplyr)
+
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 DaysMonth <- readRDS('data/DaysMonth.RDS')
 DaysMonth$declination <- 0.409*sin(2*3.141592*DaysMonth$Day_/365-1.39)
+monind <- c(12,1:12,1)
 #acquire monthly data from grids ---- 
-Lat = 43.074722
-Lon = -89.384167
-
+Lat = 39.768611; Lon = -86.158056 # Indianapolis
+Lat = 43.074722; Lon = -89.384167 # Madison
+Lat =34.05; Lon = -118.25 #Los Angeles
+Lat = 42.961111; Lon =  -85.655556 #Grand Rapids
 xy = as.matrix(as.data.frame(list(x=Lon, y=Lat)))
 xy <- vect(xy, crs="+proj=longlat +datum=WGS84")
-Elev = extract(rast(paste0('wc2.1_2.5m_elev/wc2.1_2.5m_elev.tif')), xy)[1,2]; names(Elev) <- 'Elev'
+Elev = extract(rast(paste0('wc2.1_2.5m_elev/wc2.1_2.5m_elev.tif')), xy)[1,2]; names(Elev) <- NULL
 month <- c('01','02','03','04','05','06','07','08','09','10','11','12')
 climtab <- NA
 for (i in 1:12){
   mon = month[i]
-  th = extract(rast(paste0('data/tx',month[i],'.tif')), xy)[2]; names(th) <- 'th'
-  tl = extract(rast(paste0('data/tn',month[i],'.tif')), xy)[2]; names(tl) <- 'tl'
-  p = extract(rast(paste0('data/p',month[i],'.tif')), xy)[2]; names(p) <- 'p'
-  climtab0 <- cbind(mon, th, tl, p)
+  th = extract(rast(paste0('data/tx',month[i],'.tif')), xy)[2] ; #names(th) <- 'th'
+  tl = extract(rast(paste0('data/tn',month[i],'.tif')), xy)[2] ; #names(tl) <- 'tl'
+  p = extract(rast(paste0('data/p',month[i],'.tif')), xy)[2] ; #names(p) <- 'p'
+  climtab0 <- as.data.frame(cbind(mon, th, tl, p))
+  climtab0$th <- as.numeric(climtab0$th);  climtab0$tl <- as.numeric(climtab0$tl);  climtab0$p <- as.numeric(climtab0$p)
   if(is.na(climtab)){climtab=climtab0}else{climtab <- rbind(climtab, climtab0)}
 }
-
+climtab0 <- NULL
 #Humidity ----
 climtab$t <- (climtab$th+climtab$tl)/2
 climtab$Vpmax = 0.6108*exp(17.27*climtab$th/(climtab$th+237.3)) #saturation vapor pressure kPa
@@ -41,19 +44,72 @@ for(i in 1:12){
 climtab$hs <- acos(pmin(pmax(-tan(Lat/360*2*3.141592) * tan(climtab$declination),-1),1))
 climtab$Ra <- 117.5 * (climtab$hs*sin(Lat/360*2*3.141592)*sin(climtab$declination) +
                  cos(Lat/360*2*3.141592)*cos(climtab$declination)*sin(climtab$hs)) / 3.141592
+climtab$Dl <- ifelse(Lat + climtab$declination*360/2/3.141592 > 89.16924, 24, ifelse(Lat - climtab$declination*360/2/3.141592 >= 90, 0, (atan(-((sin(-0.83/360*2*3.141592)-sin(climtab$declination)*sin(Lat/360*2*3.141592))/(cos(climtab$declination)*cos(Lat/360*2*3.141592)))/(-((sin(-0.83/360*2*3.141592)-sin(climtab$declination)*sin(Lat/360*2*3.141592))/(cos(climtab$declination)*cos(Lat/360*2*3.141592)))*((sin(-0.83/360*2*3.141592)-sin(climtab$declination)*sin(Lat/360*2*3.141592))/(cos(climtab$declination)*cos(Lat/360*2*3.141592)))+1)^0.5)+2*atan(1))/3.141592*24))
 climtab$hs <- NULL ; climtab$declination <- NULL
-climtab$Rso <- (0.75+2*10^-5*Elev)*climtab$Ra
+climtab$Rso <- (0.75+2*10^-5*Elev)*climtab$Ra 
+              pmin(climtab$Rso,pmax(0.3*climtab$Rso, 0.16*(climtab$th-climtab$tl)^0.5*climtab$Ra))
 climtab$Rs <- pmin(climtab$Rso,pmax(0.3*climtab$Rso, 0.175*(climtab$th-climtab$tl)^0.5*climtab$Ra)) # Estimate of normally measured solar radiation Rs/Rso is limited to 0.3-1 and using formula for Hargreaves with constant of 0.16 inland and 0.19 for coastal
+climtab$Rnl <- 4.901*10^-9 * (1.35*climtab$Rs/climtab$Rso-0.35) * (0.34 - 0.14 * climtab$Vpmin^0.5) * ((climtab$th+273.16)^4 + (climtab$tl+273.16)^4)/2
+climtab$Rns <- (1-0.23)*climtab$Rs
+climtab$Rn <- climtab$Rns - climtab$Rnl
+climtab$Gi = 0.07*(climtab[monind[as.numeric(rownames(climtab))+2],]$t - climtab[monind[as.numeric(rownames(climtab))],]$t)
 
-climtab$Rnl <- sigma*((climtab$th+273.16)^4 + (climtab$tl+273.16)^4)/2*(0.34-0.14*climtab$Vpmin^0.5)*(1.35*min(Rs/Rso, 1) - 0.35)
+climtab$delta <- 2503*exp(17.27*climtab$t/(climtab$t+237.3))/(climtab$t+237.3)^2
 
 
-Rnl <- sigma*(Tkmax^4 + Tkmin^4)/2*(0.34-0.14*Vd^0.5)*(1.35*min(Rs/Rso, 1) - 0.35)
-Rn <- Rns - Rnl
+climtab$lambda <- 2.501 - (2.361*10^-3)*climtab$t
+Ps <- 101.3*((293-0.0065*Elev)/293)^5.26 #kPa
+gamma = 0.000665*Ps
 
+#Thornthwaite ----
 
-climtab$e <- 0.008404*216.7*exp(17.26939*climtab$t/
+climtab$I = (pmax(0,climtab$t)/5)^1.514
+I <- sum(climtab$I); climtab$I <- NULL
+a = 0.49239+1792*10^-5*I-771*10^-7*I^2+675*10^-9*I^3
+
+climtab$e.tw = 16*(10*pmax(climtab$t,0)/I)^a*(climtab$Dl/12)*(climtab$Days/30)
+
+climtab$e.gs <- 0.008404*216.7*exp(17.26939*climtab$t/
                           (climtab$t+237.3))/(climtab$t+273.3)*(climtab$Ra)*climtab$Days*abs((climtab$th - climtab$tl))^0.5 + 0.001
+climtab$e.pt <- 1.26 * (climtab$delta / (climtab$delta + gamma))*(climtab$Rn-climtab$Gi)/climtab$lambda*climtab$Days #
+
+climtab$e.pm <- (0.408*climtab$delta*(climtab$Rn-climtab$Gi)+gamma*900/(climtab$t+273)*2*(climtab$Vp-climtab$Vpmin))/(climtab$delta+gamma*(1+0.34*2))*climtab$Days #mm asuming windspeed or 2 m/s
+
+climtab$e.hs <- 0.408*0.0023*(climtab$t+17.78)*(climtab$th-climtab$tl)^0.5*climtab$Ra*climtab$Days
+
+climtab$e.tc <- 0.01333 *((23.9001*climtab$Rs)+50)*pmax(climtab$t,0)/(pmax(climtab$t,0)+15)*(1+(50-pmin(50,climtab$RH))/70)*climtab$Days
+
+climtab <- subset(climtab, select= -c(Vp, Vpmax, Vpmin, delta, lambda, Rns, Rnl, Rso, Rs)) 
+
+
+#Thornthwaite
+sum(climtab$e.tw) 
+#Schmidt
+sum(climtab$e.gs)
+#Priestley-Taylor
+sum(climtab$e.pt)
+#Penman- Monteith
+sum(climtab$e.pm)
+#Hargreaves Samani
+sum(climtab$e.hs)
+#Turc
+sum(climtab$e.tc)
+
+totalp <- sum(climtab$p)
+#Thornthwaite
+totalp/sum(climtab$e.tw) 
+#Schmidt
+totalp/sum(climtab$e.gs)
+#Priestley-Taylor
+totalp/sum(climtab$e.pt)
+#Penman- Monteith
+totalp/sum(climtab$e.pm)
+#Hargreaves Samani
+totalp/sum(climtab$e.hs)
+#Turc
+totalp/sum(climtab$e.tc)
+
+
 
 #single month calculation
 i=7
@@ -65,48 +121,23 @@ declination = DaysMonth[i,]$declination
 hs <- acos(min(max(-tan(Lat/360*2*3.141592) * tan(declination),-1),1))
 Ra <- 117.5 * (hs*sin(Lat/360*2*3.141592)*sin(declination) +
                  cos(Lat/360*2*3.141592)*cos(declination)*sin(hs)) / 3.141592
-e <- 0.008404*216.7*exp(17.26939*t/
+e.gs <- 0.008404*216.7*exp(17.26939*t/
                           (t+237.3))/(t+273.3)*(Ra)*Days*abs((th - tl))^0.5 + 0.001
 
 
 # ------
-dr <- 1+0.033*cos(2*3.141592/365*DaysMonth[i,]$Day_)
-ws <- acos(-tan(Lat*2*3.141592/360)*tan(DaysMonth[i,]$declination))
-Rsa <- (24*60/3.141592)*0.08202*dr*(
-  cos(Lat*2*3.141592/360) * cos(DaysMonth[i,]$declination) * sin(ws) + ws * sin(Lat*2*3.141592/360)*sin(DaysMonth[i,]$declination)
-)
-# ------
-Elev <- 0 #m
+
 Ps <- 101.3*((293-0.0065*Elev)/293)^5.26 #kPa
 Vs <- 0.611*exp(17.27*t/
                   (t+237.3))
 Vd <- 0.611*exp(17.27*tl/
                   (t+237.3))
-Rns <- (1-0.23)*Ra
-sigma <- 4.903*10^-9
-Tkmax <- th+273.16
-Tkmin <- tl+273.16
-Rso <- (0.75+2*10^-5*Elev)*Ra
-Rs <- Ra
-Rnl <- sigma*(Tkmax^4 + Tkmin^4)/2*(0.34-0.14*Vd^0.5)*(1.35*min(Rs/Rso, 1) - 0.35)
-Rn <- Rns - Rnl
-delta <- 4098*Vs*t/(t+237.3)
-lambda <- 2.501 - (2.361*10^-3)*t
-gamma <- 1.013*Ps/(0.622*lambda)
 alpha <- 1.26
 e.pt <- alpha * delta / (delta + gamma) * Rn *Days
 #Turc ----
 alpha = 0.01333
 e.tc <- alpha *((23.9001*Rs)+50)*t/(t+15)*(1+(50-min(50,RH))/70)*Days
 
-#Thornthwaite ----
-I = 3*(22/5)^1.514 + 6*((22+-5)/2/5)^1.514 + 3*(0/5)^1.514
-a = 0.49239+1792*10^-5*I-771*10^-7*I^2+675*10^-9*I^3
-Ta = (22+-5)/2
-et0 = 16*(10*Ta/I)^a
-dm=31
-N=15
-e.th <- et0*(N/12)*(dm/30)
 
 #Hargreaves-Samani ----
 #Kt = 0.17
@@ -114,18 +145,11 @@ e.th <- et0*(N/12)*(dm/30)
 e.hs <- 0.0023*(T+17.78)*(th-tl)^0.5*Ra*Days
 
 #Reduced PM ----
-Rs = max(0,0.7*Ra - 4) # Estimate of normally measured solar radiation
 Ps <- 101.3*((293-0.0065*Elev)/293)^5.26 #kPa
-Vp = 0.6108*exp(17.27*t/(t+237.3)) #saturation vapor pressure kPa
 Vpmax = 0.6108*exp(17.27*th/(th+237.3)) #saturation vapor pressure kPa
 Vpmin = 0.6108*exp(17.27*tl/(tl+237.3)) #saturation vapor pressure kPa
 Vp = (Vpmax+Vpmin)/2
 RH = Vpmin/mean(Vpmax,Vpmin)*100  #Daily Relative humidity % assuming low temperature is saturated
-Gi = 0.07*(t-t) #ignore for now
-
-
-
-
 # PT ----
 alpha = 1.26
 lambda = 2.501 - 0.002361*t #MJ/kg latent heat of vaporization
@@ -137,16 +161,13 @@ Ps <- 101.3*((293-0.0065*Elev)/293)^5.26 #kPa Cp=1.013 KJ/kg/C
 gamma = 0.000665*Ps #psychrometric constant kPa/C
 Rso = (0.75 + 2*10^-5*Elev)*Ra #clear sky radiation
 #Rs = max(0.3*Rso,0.7*Ra - 4) # Estimate of normally measured solar radiation Rs/Rso is limited to 0.3-1
-Rs = min(Rso,max(0.3*Rso, 0.16*(th-tl)^0.5*Ra)) # Estimate of normally measured solar radiation Rs/Rso is limited to 0.3-1 and using formula for Hargreaves with constant of 0.16 inland and 0.19 for coastal
+Rs = min(Rso,max(0.3*Rso, 0.175*(th-tl)^0.5*Ra)) # Estimate of normally measured solar radiation Rs/Rso is limited to 0.3-1 and using formula for Hargreaves with constant of 0.16 inland and 0.19 for coastal
 Gi = 0.07*(t-t)#heat flux monthly periods previous and following months t[i+1] - t[i-1] make index to capture months in a cycle 1:14 for c(12,1:12,1); 1=m+1; m=months 1:12
 
 fcd = (1.35*Rs/Rso-0.35)
 Rns =  (1 - 0.23)*Rs #albedo of 0.23
 Rnl = 4.901*10^-9 * fcd * (0.34 - 0.14 * Vpmin^0.5) * ((th+273.16)^4 + (tl+273.16)^4)/2
 Rn = Rns - Rnl #MJ/m2/d
-Rn.pt = Rn*23.9006 #cal/m2/day
-#f = (1.2*Rs/Ra+0.1)#Rn = 0.77*Rs - 2.45*10^-9*f*(0.261*exp(-7.7710*10^-4*t^2) - 0.02)*((th+273.16)^4 + (tl+273.16)^4)+ 0.83
-#e.pt <- alpha * (delta / (delta + gamma.pt))*(Rn.pt-Gi)*Days * 10 #original in cm *10 for mm
 e.pt <- alpha * (delta / (delta + gamma))*(Rn-Gi)/lambda*Days #
 
 e.pm <- (0.408*delta*(Rn-Gi)+gamma*900/(t+273)*2*(Vp-Vpmin))/(delta+gamma*(1+0.34*2))*Days #mm asuming windspeed or 2 m/s
